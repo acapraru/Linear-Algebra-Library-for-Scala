206b5b263aa85e8a1eff50239b7b62bf5292f53a
acapraru <56480840+acapraru@users.noreply.github.com>
2020-06-16 23:45:25 +0300

    Add file

--- numMethodsTester.scala
+++ numMethodsTester.scala
@@ -3,18 +3,25 @@ object numMethTester{
   def f1(x:Double): Double = -x* Math.log(x)/Math.log(2) // integration
   def f2(x:Double): Double = Math.cos(Math.cos(x)) // integration
   def f3(x:Double): Double = Math.log(Math.log(x)) // integration
-  def f4(x:Double): Double = 10 - x*x // bisection
-  def f5(x:Double): Double = x*x - 3 // bisection
-  def f6 (x:Double):Double = x*x*x*x - 4*x*x +4 // Newton's 1D
-  def df6 (x:Double):Double = 4 *x*x*x - 8*x // Newton's 1D
-  def f7(x:Double):Double = Math.pow(x-1,50)*Math.pow(x+1,50) // Newton's 1D
-  def df7(x:Double):Double = 100*x*Math.pow(x-1,49)*Math.pow(x+1,49) // Newton's 1D
+  
+  def f4(x:Double): Double = 10 - x*x // bisection + Newton's 1D + secant
+  def df4(x:Double): Double = -2*x // Newton's 1D
+  def f5(x:Double): Double = x*x - 3 // bisection + Newton's 1D + secant
+  def df5(x:Double): Double = 2*x // Newton's 1D
+  
+  def f6 (x:Double):Double = x*x*x*x - 4*x*x +4 // Newton's 1D + secant
+  def df6 (x:Double):Double = 4 *x*x*x - 8*x // Newton's 1D + secant
+  def f7(x:Double):Double = Math.pow(x-1,50)*Math.pow(x+1,50) // Newton's 1D + secant
+  def df7(x:Double):Double = 100*x*Math.pow(x-1,49)*Math.pow(x+1,49) // Newton's 1D + secant
+  
   def f8(x:Double): Double = Math.cos(x) - x // Newton's 1D + secant
   def df8(x:Double): Double = -Math.sin(x) - 1 // Newton's 1D
   def f9(x:Double): Double = Math.pow(x,1.0/3) // Newton's 1D failing
   def df9(x:Double): Double = -1.0/3*Math.pow(x,-2.0/3) // Newton's 1D failing
+  
   def f10(x:Double): Double = x*x*x - x - 1 // secant
   def f11(x:Double):Double = Math.cos(x) + 2 * Math.sin(x) + x*x // secant
+  
   def f12(xs:Array[Double]): Array[Double] ={
     assert(xs.size == 2, "The function applies to vectors of dimension 2!")
 	val x = xs(0); val y = xs(1)
@@ -65,6 +72,7 @@ object numMethTester{
 	val z = xs(2)
 	return Array(Array(Math.exp(x),1,1),Array(1.0,2*Math.exp(2*y),1),Array(1.0,1,3*Math.exp(3*z)))
   } // Newton's + Broyden's n dimensions
+  
   def f17(x:Double): Double = Math.exp(x-1)+Math.exp(100-100*x) // Golden section search (s.br. (0,2*phi,2))
   def f18(x:Double): Double = ((x-1)*Math.sin(x))/(x*x+1) // Golden section search (s.br. (-6,-6*phi,0))
   def f19(x:Double): Double = (Math.cos(x)-1)/(x*x) // Golden section search (s.br. (-1,2*phi-1,1))
@@ -76,6 +84,7 @@ object numMethTester{
 	val maxmax = Math.max(max1,s3)
 	return maxmax
   } // Golden section search (s.br. (0,1,2))
+  
   def f21(xs:Array[Double]): Double ={
     assert(xs.size == 2, "The function applies to vectors of dimension 2!")
 	val x = xs(0)
@@ -155,23 +164,61 @@ object numMethTester{
 	return Array(4*x*x*x+1,2*y-1)
   } // Gradient descent (s.p. (0,0))
 
+  def numInttest():Unit ={
+    val u1 = new numIntegration
+	println
+	println ("Numerical integration in 1 dimension examples:")
+	println
+	
+	println("For the function f1 (-x*log(2)(x)) from 0.5 to 1 using 2 intervals:")
+	println("Midpoint:\t"+u1.midpoint1D(f1,0.5,1,2))
+	println("Trapezium:\t"+u1.trapezium1D(f1,0.5,1,2))
+	println("Simpsons:\t"+u1.simpsons1D(f1,0.5,1,2))
+	println
+	
+	println("For the function f2 (cos(cos(x))) from 0 to pi using 4 intervals:")
+	println("Midpoint:\t"+u1.midpoint1D(f2,0.0,Math.PI,4))
+	println("Trapezium:\t"+u1.trapezium1D(f2,0.0,Math.PI,4))
+	println("Simpsons:\t"+u1.simpsons1D(f2,0.0,Math.PI,4))
+	println
+	
+	println("For the function f3 (log(log(x)) from 3 to 4 using 8 intervals:")	
+	println("Midpoint:\t"+u1.midpoint1D(f3,3.0,4.0,8))
+	println("Trapezium:\t"+u1.trapezium1D(f3,3.0,4.0,8))
+	println("Simpsons:\t"+u1.simpsons1D(f3,3.0,4.0,8))
+	println
+  
+  }
+  
+  def numRFtest():Unit ={
+	val u2 = new numRootFinding
+	println
+	println("Numerical root-finding in 1 dimension examples:")
+	println
+	
+	println("For the function f4 (10-x^2):")
+	println("Bisection:\t"+u2.bisection(f4,2,10,1000,1e-14))
+	println("Newton:\t\t"+u2.newton1D(f4,df4,1,1000,1e-14))
+	println("Secant:\t\t"+u2.secant(f4,0,1,1000,1e-14)) 
+	println
+	
+	println("For the function f5 (x^2-3):")
+	println("Bisection:\t"+u2.bisection(f5,0,10,1000,1e-14))
+	println("Newton:\t\t"+u2.newton1D(f5,df5,1,1000,1e-14))
+	println("Secant:\t\t"+u2.secant(f5,0,2,1000,1e-14)) 
+	println
+	
+	println("For the function f6 (x^4-4*x^2+4):")
+	println("Newton:\t\t"+u2.newton1D(f6,df6,2,1000,1e-14))
+	println("Secant:\t\t"+u2.secant(f6,1,200,1000,1e-14)) 
+	println
+
+  }
+
   def main(args: Array[String]):Unit ={
-    val u = new numIntegration
-	
-	println("For the first function:")
-	println(u.midpoint1D(f1,0.5,1,2))
-	println(u.trapezium1D(f1,0.5,1,2))
-	println(u.simpsons1D(f1,0.5,1,2))
-	
-	println("For the second function:")
-	println(u.midpoint1D(f2,0.0,Math.PI,4))
-	println(u.trapezium1D(f2,0.0,Math.PI,4))
-	println(u.simpsons1D(f2,0.0,Math.PI,4))
-	
-	println("For the third function:")
-	println(u.midpoint1D(f3,3.0,4.0,8))
-	println(u.trapezium1D(f3,3.0,4.0,8))
-	println(u.simpsons1D(f3,3.0,4.0,8))
+	val u3 = new numOptimization
+	numInttest
+	numRFtest
   }
   
 }
